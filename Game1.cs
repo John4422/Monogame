215b350f0ea7d60e1ea37486d2964bd73885fa51
John4422 <Johnstein456@hotmail.com>
2022-11-23 08:36:46 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -10,12 +10,15 @@ namespace Monogame
         private SpriteBatch _spriteBatch;
         
         Texture2D dinoTexture;
-        Texture2D koopaTexture;
+        Texture2D KoopaTexture;
         Texture2D CasperTexture;
-        Texture2D SquirldulTexture;
+        Texture2D SquirrdulTexture;
         Texture2D backgroundTexture;
-
-
+        Texture2D circleTexture;
+        Rectangle circleRect;
+        Texture2D rectangleTexture;
+        Rectangle rectangleRect;
+        SpriteFont TitleFont;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -27,6 +30,8 @@ namespace Monogame
         {
             // TODO: Add your initialization logic here
             base.Initialize();
+            circleRect = new Rectangle(950, 10, 40, 40);
+            rectangleRect = new Rectangle(10, 10, 40, 40);
             _graphics.PreferredBackBufferWidth = 1000; 
             _graphics.PreferredBackBufferHeight = 800;
             _graphics.ApplyChanges();
@@ -39,9 +44,13 @@ namespace Monogame
 
             // TODO: use this.Content to load your game content here
             dinoTexture = Content.Load<Texture2D>("dino");
-            koopaTexture = Content.Load<Texture2D>("Koopa");
-            SquirldulTexture = Content.Load<Texture2D>("Squirldul");
+            KoopaTexture = Content.Load<Texture2D>("Koopa");
+            SquirrdulTexture = Content.Load<Texture2D>("Sqirrdul");
             CasperTexture = Content.Load<Texture2D>("Casper");
+            circleTexture = Content.Load<Texture2D>("circle");
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
+            TitleFont = Content.Load<SpriteFont>("TitleFont");
+            backgroundTexture = Content.Load<Texture2D>("background");
         }
 
         protected override void Update(GameTime gameTime)
@@ -56,18 +65,19 @@ namespace Monogame
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.SkyBlue);
+            GraphicsDevice.Clear(Color.LightBlue);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-
-            _spriteBatch.Draw(dinoTexture, new Vector2(10, 10), Color.White);
-            _spriteBatch.Draw(koopaTexture, new Vector2(800, 600), Color.White);
-            _spriteBatch.Draw(SquirldulTexture, new Vector2(600, 50), Color.White);
-            _spriteBatch.Draw(CasperTexture, new Vector2(10, 500), Color.White);
-
-
+            //_spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, 1000, 800), Color.White);
+           // _spriteBatch.Draw(dinoTexture, new Vector2(10, 10), Color.White);
+           // _spriteBatch.Draw(KoopaTexture, new Vector2(800, 600), Color.White);
+           // _spriteBatch.Draw(SquirrdulTexture, new Vector2(600, 50), Color.White);
+           // _spriteBatch.Draw(CasperTexture, new Vector2(10, 500), Color.White);
+            _spriteBatch.Draw(rectangleTexture, rectangleRect, Color.Red);
+            _spriteBatch.Draw(circleTexture, circleRect, Color.Blue);
+            _spriteBatch.DrawString(TitleFont, "John Higgs", new Vector2(300,100), Color.Pink);
             _spriteBatch.End();
 
             base.Draw(gameTime);
